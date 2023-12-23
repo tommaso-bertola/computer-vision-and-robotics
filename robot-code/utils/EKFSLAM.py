d220ef439ae9fada70b55eac327867664a9d4f99
Tommaso Bertola <bertola.tommaso@hotmail.it>
2023-12-23 19:12:49 +0100

    Improving EKFSLAM

--- robot-code/utils/EKFSLAM.py
+++ robot-code/utils/EKFSLAM.py
@@ -43,6 +43,7 @@ class EKFSLAM:
 
     def predict(self, l, r):
         # get current poition in world coordinates
+        # TODO check coordinate frames (Robot or World?)
         x, y, theta, std = self.get_robot_pose()
 
         w = self.WIDTH  # robot width from center of the two wheels
@@ -72,6 +73,7 @@ class EKFSLAM:
             R_w_2 = R+(w/2)
             x_prime = x + R_w_2*(np.sin(theta+alpha)-sin_theta)
             y_prime = y + R_w_2*(-np.cos(theta+alpha)+cos_theta)
+            # TODO: check if theta in -pi pi is needed
             theta_prime = (theta+alpha) % (2*np.pi)
 
             const_AB = (w*r)/(r-l)**2
@@ -96,7 +98,7 @@ class EKFSLAM:
         sigma_xm = self.Sigma[0:3, 3:]
         sigma_mm = self.Sigma[3:, 3:]
 
-        Sigma_top_left = G@sigma_xx@G.T + V @ self.Sigma_u @ V.T
+        Sigma_top_left = G@sigma_xx@G.T + V @ self.Sigma_u @ V.T  # final dim 3x3
         Sigma_top_right = G@sigma_xm
         Sigma_low_left = (G@sigma_xm).T
 
@@ -109,94 +111,102 @@ class EKFSLAM:
 
     def add_landmark(self, position: tuple, id: str):
         # measurement: tuple, id: str): # changed to a shorter signature of the function
-        if int(id) < 1000:
-            x, y = position
+        if int(id) <= 1000:
+            x, y = position  # array with x and y
 
             # extend self.mu and self.Sigma with the new landmark
             self.mu = np.append(self.mu, [x, y])
-            self.mu_prime = self.mu.copy()
+            self.mu_prime = np.append(self.mu_prime, [x, y])
 
             dim = self.Sigma.shape
+            dim_prime = self.Sigma.shape
             Sigma = np.zeros(np.add(dim, 2))
+            Sigma_prime = np.zeros(np.add(dim_prime, 2))
+
             Sigma[:dim[0], :dim[1]] = self.Sigma
             Sigma[-2, -2] = self.SIGMA_SQUARED_X
             Sigma[-1, -1] = self.SIGMA_SQUARED_Y
 
+            Sigma_prime[:dim[0], :dim[1]] = self.Sigma_prime
+            Sigma_prime[-2, -2] = self.SIGMA_SQUARED_X
+            Sigma_prime[-1, -1] = self.SIGMA_SQUARED_Y
+
             self.Sigma = Sigma
-            self.Sigma_prime = Sigma
+            self.Sigma_prime = Sigma_prime
 
             # add the landmark id to self.ids
             self.ids.append(id)
 
     def correction(self, landmark_position_measured: tuple, id: int):
-        # # index of identified aruco
-        # index = self.ids.index(id)
+        # index of identified aruco
+        index = self.ids.index(id)
 
-        # # current world position of robot
-        # x, y, theta, _ = self.get_robot_pose()
+        # current world position of robot
+        x, y, theta, _ = self.get_robot_pose()
 
-        # # the measured position fo the aruco from the camera
-        # r_i, beta_i = landmark_position_measured
+        # the measured position fo the aruco from the camera
+        r_i, beta_i = landmark_position_measured
 
-        # # the old position of the markers
-        # x_m, y_m = self.mu[3+index:3+index+2]
+        # the old position of the markers
+        # mu = [0,0,0, id1, id1, id2, id2, ..., idn, idn]
+        x_m, y_m = self.mu[3+2*index:3+2*index+2]
 
-        # # define the return values of h
-        # r = np.sqrt((x_m-x)**2+(y_m-y)**2)
-        # beta = self.subtract(np.arctan2((y_m-y), (x_m-x)), theta)
+        # define the return values of h
+        r = np.sqrt((x_m-x)**2+(y_m-y)**2)
+        beta = self.subtract(np.arctan2((y_m-y), (x_m-x)), theta)
 
-        # # define the entries of jacobian
-        # r_x = -(x_m-x)/r
-        # r_y = -(y_m-y)/r
-        # r_theta = 0
-        # r_x_m = -r_x
-        # r_y_m = -r_y
-        # beta_x = (y_m-y)/r**2
-        # beta_y = -(x_m-x)/r*2
-        # beta_theta = -1
-        # beta_x_m = -beta_x
-        # beta_y_m = -beta_y
+        # define the entries of jacobian
+        r_x = -(x_m-x)/r
+        r_y = -(y_m-y)/r
+        r_theta = 0
+        r_x_m = -r_x
+        r_y_m = -r_y
+        beta_x = (y_m-y)/r**2
+        beta_y = -(x_m-x)/r*2
+        beta_theta = -1
+        beta_x_m = -beta_x
+        beta_y_m = -beta_y
 
-        # H_s_robot = np.array([[r_x, r_y, r_theta,],
-        #                      [beta_x, beta_y, beta_theta,]])
-        # H_s_marker = np.array([[r_x_m, r_y_m],
-        #                       [beta_x_m, beta_y_m]])
+        H_s_robot = np.array([[r_x, r_y, r_theta,],
+                             [beta_x, beta_y, beta_theta,]])
+        H_s_marker = np.array([[r_x_m, r_y_m],
+                              [beta_x_m, beta_y_m]])
 
-        # n = 2*index
-        # m = max(3+2*len(self.ids)-n-5, 0)
+        n = 2*index
+        m = max(3+2*len(self.ids)-n-5, 0)
 
-        # H = np.block([H_s_robot, np.zeros((2, n)),
-        #              H_s_marker, np.zeros((2, m))])
-        # H_s = np.block([H_s_robot, H_s_marker])
+        H = np.block([H_s_robot, np.zeros((2, n)),
+                     H_s_marker, np.zeros((2, m))])
+        H_s = np.block([H_s_robot, H_s_marker])
 
-        # inf = 3+2*index
-        # sup = 3+2*index+2
+        inf = 3+2*index
+        sup = 3+2*index+2
 
-        # sigma_s_top_left = self.Sigma[0:3, 0:3]
-        # sigma_s_low_right = self.Sigma[inf:sup, inf:sup]
-        # sigma_s_top_right = self.Sigma[0:3, inf:sup]
-        # sigma_s_low_left = self.Sigma[inf:sup, 0:3]
+        sigma_s_top_left = self.Sigma[0:3, 0:3]
+        sigma_s_low_right = self.Sigma[inf:sup, inf:sup]
+        sigma_s_top_right = self.Sigma[0:3, inf:sup]
+        sigma_s_low_left = self.Sigma[inf:sup, 0:3]
 
-        # sigma_s = np.block([[sigma_s_top_left, sigma_s_top_right],
-        #                     [sigma_s_low_left, sigma_s_low_right]])
+        sigma_s = np.block([[sigma_s_top_left, sigma_s_top_right],
+                            [sigma_s_low_left, sigma_s_low_right]])
 
-        # Z = H_s@sigma_s@(H_s.T)+self.Q
+        Z = H_s@sigma_s@(H_s.T)+self.Q
 
-        # K = self.Sigma_prime@(H.T)@(np.linalg.inv(Z))
-        # self.mu = self.mu_prime + \
-        #     K@(np.array([r_i-r, self.subtract(beta_i, beta)]).T)
-        # self.Sigma = (np.eye(3+2*len(self.ids))-K@H)@self.Sigma_prime
-        pass
+        K = self.Sigma_prime@(H.T)@(np.linalg.inv(Z))
+        self.mu = self.mu_prime + \
+            K@(np.array([r_i-r, self.subtract(beta_i, beta)]).T)
+        self.Sigma = (np.eye(3+2*len(self.ids))-K@H)@self.Sigma_prime
+        # pass
 
     def get_robot_pose(self):
         # read out the robot position and angle from mu variable
         # read out robot error from Sigma
         robot_x, robot_y, robot_theta = self.mu[:3].copy()
         sigma = self.Sigma[:2, :2]
-        var_x, var_y, _ = self.get_error_ellipse(sigma)
-        robot_stdev = [np.sqrt(var_x), np.sqrt(var_y)]
+        var_x, var_y, angle = self.get_error_ellipse(sigma)
+        robot_stdev = [np.sqrt(var_x), np.sqrt(var_y), angle]
 
-        return robot_x, robot_y, robot_theta, robot_stdev
+        return robot_x, robot_y, robot_theta, robot_stdev  # wrt world
 
     def get_landmark_poses(self):
         pass
